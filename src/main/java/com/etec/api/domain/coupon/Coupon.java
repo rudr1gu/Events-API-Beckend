088ebb936234899ba633e3bb31ffb143ef07586a
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-09-01 12:01:20 -0300

    :sparkles: feat: Add Coupon entity

---
+++ src/main/java/com/etec/api/domain/coupon/Coupon.java
@@ -0,0 +1,38 @@
+package com.etec.api.domain.coupon;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.etec.api.domain.events.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
