db2b73c761a3281cad7189d8e85f071908a10377
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-09-01 18:36:21 -0300

    :construction: feat: Add Flyway plugin configuration for database migration

--- src/main/resources/db/migrations/V1__create-event-table.sql
+++ src/main/resources/db/migrations/V1__create-event-table.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE event (
-    id UUID get_radom_uuid() PRIMARY KEY,
+    id UUID gen_radom_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(255) NOT NULL,
     img_url VARCHAR(100) NOT NULL,
